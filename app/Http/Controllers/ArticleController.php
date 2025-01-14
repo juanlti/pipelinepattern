c0d0f40a847e1d986c018e34179526e09c7b7e4d
juanc <juancruzliberati@hotmail.com>
2025-01-14 19:11:43 -0300

    version 2

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,14 +9,8 @@ class ArticleController extends Controller
 {
     public function __invoke():Renderable{
         //cuando tenga el valor de status, se ejecutara la funcion
-        $articles = Article::query()
-            ->when(request()->query("status"),function($query){
-                $query->where("status",request()->query("status"));
-        })
-            ->when(request()->query("sort"),function($query) {
-                //cuando tenga el valor de sort, se ejecutara la funcion
-                $query->orderBy("id", request()->query("sort"));
-            })
+        $articles = Article::filtered()
+
             ->paginate()
             ->withQueryString();
 
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,12 @@ class Article extends Model
 {
     /** @use HasFactory<\Database\Factories\ArticleFactory> */
     use HasFactory;
+
+    public function scopeFiltered(Builder $builder):Builder{
+        return $builder
+        ->when(request('search'),function($query){
+            $query->where('title','like','%'.request('search').'%')
+                ->orWhere('body','like','%'.request('search').'%');
+        });
+    }
 }
